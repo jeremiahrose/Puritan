8a0b7caed8e17f9758e01e6c934c970d0cb416d6
Jeremiah Rose <jeremiah.aaron.rose@gmail.com>
2019-06-10 10:01:50 +0000

    remove helper scripts

--- make-linuxmenuconfig.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-set -x
-
-make linux-menuconfig
-cp output/build/linux-rpi-4.18.y/.config ../br-external/configs/kernel_config
--- save.sh
+++
@@ -1,4 +0,0 @@
-echo "saving Buildroot defconfig"
-make savedefconfig
-echo "saving Kernel defconfig"
-make linux-update-defconfig
