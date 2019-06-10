702cfb922cbae1039ec79fb2f4741088ddd9c9e6
Jeremiah Rose <jeremiah.aaron.rose@gmail.com>
2019-06-10 11:31:38 +1000

    skeleton of puredata package

---
+++ br-external/external.mk
@@ -0,0 +1,1 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_TPT6_PATH)/package/*/*.mk))
---
+++ br-external/package/puredata/puredata.mk
@@ -0,0 +1,3 @@
+PUREDATA_VERSION = 0.49-0
+PUREDATA_SITE = https://github.com/pure-data/pure-data/archive/
+PUREDATA_SOURCE = $(PUREDATA_VERSION).tar.gz
