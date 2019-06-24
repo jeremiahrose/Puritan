fe1e8dbccb77ce68ee0a693b980d9375bfa0825d
Jeremiah Rose <jeremiah.aaron.rose@gmail.com>
2019-06-24 10:10:32 +0000

    disis_spi: new package

---
+++ br-external/package/disis_spi/disis_spi.mk
@@ -0,0 +1,19 @@
+DISIS_SPI_VERSION = 1.0
+DISIS_SPI_SITE = ../br-external/package/disis_spi
+DISIS_SPI_SITE_METHOD = local
+DISIS_SPI_DEPENDENCIES = wiringpi puredata
+
+define DISIS_SPI_BUILD_CMDS
+	#$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+	$(TARGET_CC) $(TARGET_CFLAGS) -o $(@D)/disis_spi.o -c $(@D)/disis_spi.c
+	$(TARGET_LD) $(TARGET_LDFLAGS) -shared -o $(@D)/disis_spi.l_arm $(@D)/disis_spi.o -lc -lm $(APPENDFLAGS)
+	$(TARGET_STRIP) --strip-unneeded $(@D)/disis_spi.l_arm
+	rm $(@D)/disis_spi.o
+	mv $(@D)/disis_spi.l_arm $(@D)/disis_spi.pd_linux
+endef
+
+define DISIS_SPI_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/disis_spi.pd_linux $(TARGET_DIR)/usr/local/lib/pd-externals/disis_spi.pd_linux
+endef
+
+$(eval $(generic-package))
--- br-external/package/puredata/puredata.mk
+++ br-external/package/puredata/puredata.mk
@@ -2,13 +2,12 @@ PUREDATA_VERSION = 0.49-0
 PUREDATA_SITE = https://github.com/pure-data/pure-data/archive
 PUREDATA_SOURCE = $(PUREDATA_VERSION).tar.gz
 PUREDATA_AUTORECONF = YES
+PUREDATA_INSTALL_STAGING = YES
 
 define PUREDATA_POST_EXTRACT_FIXUP
         mkdir -p output/build/puredata-$(PUREDATA_VERSION)/m4/generated
 endef
 
 PUREDATA_POST_EXTRACT_HOOKS += PUREDATA_POST_EXTRACT_FIXUP
-
-include $(sort $(wildcard package/puredata/pd-wiringpi/*.mk))
 
 $(eval $(autotools-package))
