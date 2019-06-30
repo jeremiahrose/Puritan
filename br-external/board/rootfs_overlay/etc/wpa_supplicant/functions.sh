5be68eb2730fdcade82ec93c273aa5660a88379f
Jeremiah Rose <jeremiah.aaron.rose@gmail.com>
2019-06-30 18:15:31 +0000

    Revert "always use ifup -f"
    This reverts commit 73e713bfd5fe8ccb090cb3e4b6c192f355efa46f.

--- br-external/board/rootfs_overlay/etc/wpa_supplicant/functions.sh
+++ br-external/board/rootfs_overlay/etc/wpa_supplicant/functions.sh
@@ -961,9 +961,12 @@ ifup () {
 
 		ifupdown_lock
 
-		# Busybox ifup has to be forced with -f
-		/sbin/ifup -v -f "$WPA_IFACE=$WPA_LOGICAL_IFACE"
-
+		if [ -n "$IFSTATE_FILE" ] && grep -q "^$WPA_IFACE=$WPA_IFACE" "$IFSTATE_FILE"; then
+			# Force settings over the unconfigured "master" IFACE
+			/sbin/ifup -v --force "$WPA_IFACE=$WPA_LOGICAL_IFACE"
+		else
+			/sbin/ifup -v "$WPA_IFACE=$WPA_LOGICAL_IFACE"
+		fi
 		IFUP_RETVAL="$?"
 
 		ifupdown_unlock
